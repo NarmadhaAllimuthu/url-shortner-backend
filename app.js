675cfc68987bba015e4fca96b7906d3c171241c9
NarmadhaAllimuthu <narmadhaallimuthu006@gmail.com>
2023-12-28 19:54:06 +0530

    update now

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const urlShortRouter = require("./routes/urlShort");
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
